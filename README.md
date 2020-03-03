073e1fac1c83af8eae8c6341d4e244fb00168bd2
zbremmer <bremmezm@gmail.com>
2020-03-02 19:50:27 -0800

    Add init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Forecast
 A convenience class for forecasting tasks
+
+The goal of this class is to simplify tasks of forecasting. Users should be able to get up to speed quickly using the 
+methods of the class. This is not meant to replace more advanced functionality of timeseries analysis, but rather is a 
+way to explore the data, quickly prototype and test models, and determine which direction more in-depth analysis should 
+take. 
+
+
+## Notes
+- How will this handle data at different time granularities (hourly, monthly, annual, etc)?
+
+
+## Todo
+- Build plotting capabilities using altair. Should include timeseries plotting and diagnostics (ACF/PACF), decomp, etc.
+- Restructure so everything no in util
+- Move test / train split and some feature generation (lags, scaling) to models
+- Add models (SARIMAX, prophet?, surprise?)
---
+++ forecast/forecast.py
@@ -0,0 +1,114 @@
+from forecast import _pipeline, _util, _models
+
+
+
+class Forecast:
+    """
+    This class contains everything needed to perform forecasting. It was created to simplify the
+    forecast modeling process and allow for the entire object to be saved as an artifact, including the raw data,
+    model, forecasts, and other attributes.
+
+    A class instance represents a single time series to be forecasted.
+    TODO: Expand functionality to include multiple dynamic forecasting
+
+    The data passed into the class should be a pandas dataframe structured as follows:
+
+    1. The dataframe should have a datetime index
+    2. The dataframe should have a single target column named "y"
+    3. The dataframe can include additional features. Columns with a datatype of 'category' or 'object' will be treated
+       as categorical features.
+
+     TODO: Error handling, probably using decorators as described in
+     https://code.tutsplus.com/tutorials/professional-error-handling-with-python--cms-25950
+
+    Attributes
+    ------------
+    data : pandas.DataFrame
+        A dataframe of the data to be used for forecasting
+    start_date : str
+        The start date of the forecast period
+    end_date : str
+        The end date of the forecast period
+    lags : int
+        The number of lags to generate for supervised learning (default = 90)
+        TODO: This should be found using automated search in future versions of project
+
+    """
+
+    def __init__(self,df, start_date, end_date, lags: int = 90):
+
+        self.data = df
+        self.start_date = start_date
+        self.end_date = end_date
+        self.lags = lags
+        self.agg_data = None
+        self.train_data = None
+        self.pred_data = None
+
+
+
+    def clean_data(self):
+        """
+        This method adds days to the data series in order to ensure completeness. Future cleaning steps can be
+        added in this method. Note that the add_days() method makes changes to the data attribute of a
+        class instance in place.
+
+        When a day is added, 0 is added to each numeric column and NaN is left in each categorical variable. This
+        is done to prevent aggregation issues in generate_features(). The new 'NaN' categories will be dropped during
+        feature generation.
+
+        #TODO: Add completeness for data at different granularities (annual, monthly, hourly, etc.)
+
+        :return:
+        """
+        try:
+            _util.add_days(self)
+            return True
+        except Exception as e:
+            return e
+
+
+    def generate_features(self):
+        """
+        This method generates default features for the dataset.
+
+        TODO: Users should be able to specify which columns are one hot encoded, which are scaled, which are
+               weighted, etc.... But if none are specified, then we do it to all? << Scaling should be done by model>>
+
+        In addition, this method will aggregate data to a daily total. There may be need to omit this in the future
+        for different use cases or forecasting methods.
+
+        Note that when a day is added in clean_data(), 0 is added to each numeric column and NaN is added to each
+        categorical variable. This is done to prevent aggregation issues in generate_features(). The new 'NaN'
+        categories will be dropped during feature generation.
+
+        :return:
+        """
+        try:
+            _util.aggregate_data(self)
+            _util.split_train_pred(self)
+            _util.get_lags(self)
+            _util.clean_pred(self)
+            _util.get_day_features(self)
+
+            # Replace above with encode_weights() and one_hot_encode()
+
+            return True
+
+        except Exception as e:
+            return e
+
+
+    def fit_model(self, model_name):
+        """
+
+        :param model_name:
+        :return:
+        """
+
+        _util.test_train_split(self)
+        self.model = _util.get_model(model_name)
+        self.model.fit()
+
+        ### If the model is supervised, then should go through supervised data conversion. If it's ARIMA, etc, then
+        # should go through converting to a true timeseries
